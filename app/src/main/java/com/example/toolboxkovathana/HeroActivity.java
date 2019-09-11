3800edd5a7d4786fdf5fb4a9bc43150e25a53c9a
kovathana5608 <kovathana5608@students.d211.org>
2019-09-11 13:44:23 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/toolboxkovathana/HeroActivity.java
@@ -0,0 +1,17 @@
+package com.example.toolboxkovathana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HeroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hero);
+    }
+
+    //https://stackoverflow.com/questions/2974862/changing-imageview-source
+    // This shows how to paste images
+}
