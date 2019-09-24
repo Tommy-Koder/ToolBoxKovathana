cfa0fbf44fa965a189f0e5dd1bf06673648350b7
Tommy Kovathana <tkovathana@yahoo.com>
2019-09-24 06:32:24 -0500

    Scroll View Implemented

--- app/src/main/java/com/example/toolboxkovathana/MainActivity.java
+++ app/src/main/java/com/example/toolboxkovathana/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.toolboxkovathana;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.character);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.characters_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
