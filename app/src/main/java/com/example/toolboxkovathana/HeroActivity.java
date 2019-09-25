49a1afdb1356ab4c52c4ac32276923f8ca885c14
Tommy Kovathana <tkovathana@yahoo.com>
2019-09-24 19:16:43 -0500

    Finished Displaying Everything

--- app/src/main/java/com/example/toolboxkovathana/HeroActivity.java
+++ app/src/main/java/com/example/toolboxkovathana/HeroActivity.java
@@ -2,16 +2,83 @@ package com.example.toolboxkovathana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HeroActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hero);
+
+        Intent intent = getIntent();
+        Boolean helm = intent.getBooleanExtra(HELM, false);
+        Boolean legs = intent.getBooleanExtra(LEGS, false);
+        Boolean armo = intent.getBooleanExtra(ARMO, false);
+        Boolean shoe = intent.getBooleanExtra(SHOE, false);
+
+        Boolean bana = intent.getBooleanExtra(BANA, false);
+        Boolean bagu = intent.getBooleanExtra(BAGU, false);
+        Boolean pick = intent.getBooleanExtra(PICK, false);
+
+        Boolean shed = intent.getBooleanExtra(SHED, false);
+
+        int chara = (int) intent.getIntExtra(CHAR, 0);
+
+        if(helm)
+            findViewById(R.id.helmetPic).setVisibility(View.VISIBLE);
+        if(legs)
+            findViewById(R.id.leggingsPic).setVisibility(View.VISIBLE);
+        if(armo)
+            findViewById(R.id.armorPic).setVisibility(View.VISIBLE);
+        if(shoe)
+            findViewById(R.id.shoesPic).setVisibility(View.VISIBLE);
+
+        if(bana) {
+            findViewById(R.id.weaponPic).setVisibility(View.VISIBLE);
+            findViewById(R.id.weaponPic).setBackgroundResource(R.drawable.banana);
+        }
+        if(bagu) {
+            findViewById(R.id.weaponPic).setVisibility(View.VISIBLE);
+            findViewById(R.id.weaponPic).setBackgroundResource(R.drawable.baguette);
+        }
+        if(pick) {
+            findViewById(R.id.weaponPic).setVisibility(View.VISIBLE);
+            findViewById(R.id.weaponPic).setBackgroundResource(R.drawable.pickle);
+        }
+
+        if(shed)
+            findViewById(R.id.shieldPic).setVisibility(View.VISIBLE);
+
+        if(chara == 0)
+            findViewById(R.id.characterPic).setBackgroundResource(R.drawable.shrek);
+        if(chara == 1)
+            findViewById(R.id.characterPic).setBackgroundResource(R.drawable.sans);
+        if(chara == 2)
+            findViewById(R.id.characterPic).setBackgroundResource(R.drawable.bernie_sanders);
+
     }
 
     //https://stackoverflow.com/questions/2974862/changing-imageview-source
     // This shows how to paste images
+
+    public static final String HELM = "helm";
+    public static final String LEGS = "legs";
+    public static final String ARMO = "armo";
+    public static final String SHOE = "shoe";
+
+    public static final String BANA = "bana";
+    public static final String BAGU = "bagu";
+    public static final String PICK = "pick";
+
+    public static final String SHED = "shed";
+
+    public static final String CHAR = "char";
+
+
+
 }
--- app/src/main/java/com/example/toolboxkovathana/MainActivity.java
+++ app/src/main/java/com/example/toolboxkovathana/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.toolboxkovathana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +26,50 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
     }
+
+
+    public void createHero(View v)
+    {
+        CheckBox helmet = (CheckBox) findViewById(R.id.checkBoxHelmet);
+        CheckBox leggings = (CheckBox) findViewById(R.id.checkBoxLeggings);
+        CheckBox armor = (CheckBox) findViewById(R.id.checkBoxArmor);
+        CheckBox shoes = (CheckBox) findViewById(R.id.checkBoxShoes);
+
+        RadioButton banana = (RadioButton) findViewById(R.id.wBanana);
+        RadioButton baguette = (RadioButton) findViewById(R.id.wBaguette);
+        RadioButton pickle = (RadioButton) findViewById(R.id.wPickle);
+
+        Switch shield = (Switch) findViewById(R.id.shieldBoolean);
+
+        Spinner character = (Spinner) findViewById(R.id.character);
+
+    /*
+        if(helmet.isChecked())
+            findViewById(R.id.helmetPic).setBackgroundResource(R.drawable.helmet);
+*/
+        Intent intent = new Intent(this, HeroActivity.class);
+
+        intent.putExtra(HeroActivity.HELM, helmet.isChecked());
+        intent.putExtra(HeroActivity.LEGS, leggings.isChecked());
+        intent.putExtra(HeroActivity.ARMO, armor.isChecked());
+        intent.putExtra(HeroActivity.SHOE, shoes.isChecked());
+
+        intent.putExtra(HeroActivity.BANA, banana.isChecked());
+        intent.putExtra(HeroActivity.BAGU, baguette.isChecked());
+        intent.putExtra(HeroActivity.PICK, pickle.isChecked());
+
+        intent.putExtra(HeroActivity.SHED, shield.isChecked());
+
+        int charNum = character.getSelectedItemPosition();
+
+        intent.putExtra(HeroActivity.CHAR, charNum);
+
+        startActivity(intent);
+    }
+
+
+
+
 }
